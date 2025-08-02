39993b415b5e8c264901e26ed8efea425a416395
Periyasamy <periyasamyr07@gmail.com>
2025-08-02 10:27:01 +0530

    demo for for_each

--- Demo/aws-demo/iam-user/main.tf
+++ Demo/aws-demo/iam-user/main.tf
@@ -1,6 +1,16 @@
-resource "aws_iam_user" "user_creation" {
-    name = var.users[count.index]
+# resource "aws_iam_user" "user_creation" {
+#     name = var.users[count.index]
 
-    count = length(var.users)
+#     count = length(var.users)
+
+# }
+
+resource "aws_iam_user" "user_creation" {
+    name = each.value
+
+    for_each = toset(var.users)
 
 }
+
+
+
